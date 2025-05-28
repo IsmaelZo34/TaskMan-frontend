09a1fe38d4b362d338816a3a7185c55017496c84
IsmaelZo34 <zo.ismael027@gmail.com>
2025-05-28 16:59:39 +0200

    nouvelle modification de l'endpoint pour coinsider avec celle de Railway

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
-const API_URL = "https://taskman-backend.railway.app/api";
+const API_URL = "https://taskman.railway.app/api";
 let token = null;
 export function setToken(t){
-    token = t;
+    token = t;  ààà
 }
 export async function login(email, password){
     const res = await fetch(`${API_URL}/login`, {
